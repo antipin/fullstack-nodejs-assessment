f5436b4a7311f7c17b972176d9e1f246043577c8
Alex Antipin <alex@antipin.com>
2024-11-04 01:11:51 +0100

    feat(backend): add "/cocktails/:id" endpoint
    "/cocktails/:id" allows to retrieve a paerticular coctail info

--- backend/src/cocktails/cocktails.controller.ts
+++ backend/src/cocktails/cocktails.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Param, ParseIntPipe, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { Cocktails } from './cocktails.entity';
 import { CocktailsService } from './cocktails.service';
 
@@ -13,9 +13,18 @@ export class CocktailsController {
 
   @Post()
   async newCocktail(@Body() cocktail: Cocktails) {
-    console.log("info: creating cocktail", cocktail)
+    console.log("info: creating cocktail", cocktail);
     const res = await this.cocktailsService.create(cocktail);
     console.log("res", res);
     return true;
+  }
+
+  @Get(':cocktailId')
+  async findCocktail(@Param('cocktailId', ParseIntPipe) cocktailId: number): Promise<Cocktails> {
+    const cocktail = await this.cocktailsService.findOne(cocktailId);
+    if (!cocktail) {
+      throw new NotFoundException(`Cocktail #${cocktailId} not found`);
+    }
+    return cocktail;
   }
 }
