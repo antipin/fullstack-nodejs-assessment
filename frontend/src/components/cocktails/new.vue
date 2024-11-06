62db73c26e183231ba897029870c0c99be9a28b4
Alex Antipin <alex@antipin.com>
2024-11-06 11:04:19 +0100

    feat(frontend): new cocktail form error handling

--- frontend/src/components/cocktails/new.vue
+++ frontend/src/components/cocktails/new.vue
@@ -1,20 +1,22 @@
 <template>
   <div>
     <form @submit.prevent="submitForm">
+      <span v-if="errors.global" class="message message--type_error">{{ errors.global }}</span>
+      <span v-if="successMessage" class="message message--type_success">{{ this.successMessage }}</span>
       <div>
         <label for="title">Title:</label>
-        <!-- <input type="text" v-model="form.title" id="title" required> -->
         <input type="text" v-model="form.title" id="title">
+        <span v-if="errors.fields.title" class="field-error">{{ errors.fields.title }}</span>
       </div>
       <div>
         <label for="price">Price:</label>
-        <!-- <input type="number" v-model="form.price" id="price" required> -->
         <input type="number" v-model="form.price" id="price">
+        <span v-if="errors.fields.price" class="field-error">{{ errors.fields.price }}</span>
       </div>
       <div>
         <label for="description">Description:</label>
-        <!-- <textarea v-model="form.description" id="description" required></textarea> -->
         <textarea v-model="form.description" id="description"></textarea>
+        <span v-if="errors.fields.description" class="field-error">{{ errors.fields.description }}</span>
       </div>
       <button type="submit">Submit</button>
     </form>
@@ -30,11 +32,26 @@ export default {
         title: '',
         price: '',
         description: ''
-      }
+      },
+      successMessage: '',
+      errors: {
+        // For global errors
+        global: null,
+        // For field-specific errors
+        fields: {},
+      },
     };
   },
   methods: {
     async submitForm() {
+
+      // Reset errors
+      this.errors = {
+        global: null,
+        fields: {},
+      };
+      this.successMessage = '';
+
       try {
         const response = await fetch('http://localhost:3000/cocktails', {
           method: 'POST',
@@ -44,19 +61,33 @@ export default {
           body: JSON.stringify(this.form)
         });
 
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
+        if (response.ok) {
+          const data = await response.json();
+          // Clear the form
+          this.form.title = '';
+          this.form.price = '';
+          this.form.description = '';
+          // Set success message
+          this.successMessage = `Cocktail "${this.form.title}" succesfully added`;
+        } else {
+          const errorData = await response.json();
+          // Handle global error
+          if (errorData.statusCode && errorData.message && typeof errorData.message === 'string') {
+            this.errors.global = errorData.message;
+          }
+          // Handle field-specific errors
+          if (Array.isArray(errorData.message)) {
+            errorData.message.forEach((fieldError) => {
+              const field = fieldError.field;
+              const errors = fieldError.errors;
+              if (field && errors && errors.length > 0) {
+                this.errors.fields[field] = errors.join(', ');
+              }
+            });
+          }
         }
-
-        const data = await response.json();
-        console.log('Form submitted successfully:', data);
-        // Clear the form
-        this.form.title = '';
-        this.form.price = '';
-        this.form.description = '';
       } catch (error) {
-        console.error('There was an error submitting the form:', error);
-        // Handle the error (e.g., show an error message)
+        this.errors.global = error.message;
       }
     }
   }
@@ -90,5 +121,26 @@ button {
 }
 button:hover {
   background-color: #0056b3;
+}
+.message {
+  display: block;
+  padding: .75rem 1rem;
+  margin-bottom: .75rem;
+}
+.message--type_success {
+  color: #155724;
+  background-color: #d4edda;
+  border-color: #c3e6cb;
+}
+.message--type_error {
+  color: #721c24;
+  background-color: #f8d7da;
+  border-color: #f5c6cb;
+}
+.field-error {
+  display: block;
+  text-transform: capitalize;
+  color: #ea0117;
+  padding: .25rem 0 .75rem;
 }
 </style>
